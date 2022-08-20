afff30e30ffb0c96740e7bb8b440b99404fc3822
Judori02 <v.steblev2002@gmail.com>
2022-08-20 18:58:17 +0300

    Добавил контекст базы данных

---
+++ src/Todo.Web/Data/Base/IBaseDbContext.cs
@@ -0,0 +1,45 @@
+﻿namespace Todo.Web.Data.Base
+{
+    public interface IBaseDbContext
+    {
+        /// <summary>
+        /// Сохраняет все изменения, сделанные в этом контексте, в базу данных.
+        /// </summary>
+        /// <returns></returns>
+        int SaveChanges();
+        /// <summary>
+        /// Сохраняет все изменения, сделанные в этом контексте, в базу данных.
+        /// </summary>
+        /// <param name="acceptAllChangesOnSuccess">
+        /// Указывает, вызывается ли <seecref="M:Microsoft.EntityFrameworkCore.ChangeTracking.ChangeTracker.AcceptAllChanges" /> после внесения изменений.
+        /// успешно отправлено в базу данных.
+        /// </param>
+        /// <returns></returns>
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        /// <summary>
+        /// Асинхронный метод. Сохраняет все изменения, сделанные в этом контексте, в базу данных.
+        /// </summary>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync();
+        /// <summary>
+        /// Асинхронный метод. Сохраняет все изменения, сделанные в этом контексте, в базу данных.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// <see cref="T:System.Threading.CancellationToken" /> распространяет уведомление о том, что операции следует отменить
+        /// </param>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = new());
+        /// <summary>
+        /// Асинхронный метод. Сохраняет все изменения, сделанные в этом контексте, в базу данных.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// <see cref = "T:System.Threading.CancellationToken" /> распространяет уведомление о том, что операции следует отменить
+        /// </param>
+        /// <param name="acceptAllChangesOnSuccess">
+        /// Указывает, вызывается ли <seecref="M:Microsoft.EntityFrameworkCore.ChangeTracking.ChangeTracker.AcceptAllChanges" /> после внесения изменений.
+        /// успешно отправлено в базу данных.
+        /// </param>
+        /// <returns></returns>
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new());
+    }
+}
---
+++ src/Todo.Web/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Todo.Web.Data.Base;
+using Todo.Web.Data.Entities;
+
+namespace Todo.Web.Data.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<User>, IBaseDbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; } = null!;
+        public DbSet<Tag> Tags { get; set; } = null!;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public int SaveChanges()
+        {
+            return base.SaveChanges();
+        }
+        public int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new())
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        public async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = new())
+        {
+            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+    }
+}
