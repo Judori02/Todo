e95923e3b58f65ab21620bdcc5741de90af78ab9
Judori02 <v.steblev2002@gmail.com>
2022-08-20 23:44:54 +0300

    Исправил ошибку в дате регистрации

--- src/Todo.Web/Data/Entities/User.cs
+++ src/Todo.Web/Data/Entities/User.cs
@@ -6,7 +6,7 @@ namespace Todo.Web.Data.Entities
     {
         public string? Name { get; set; }
         public string? Surname { get; set; }
-        public DateTime RegsiterdAt { get; set; }
+        public DateTime RegisteredAt { get; set; }
         public DateTime LastLogin { get; set; }
         public byte[]? Profile { get; set; }
     }
