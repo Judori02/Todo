713e927ed5f13a49f5aec94bc239cf1d4fad64cb
Judori02 <v.steblev2002@gmail.com>
2022-08-20 23:44:23 +0300

    Добавил конфигурацию user

--- src/Todo.Web/Data/Contexts/ApplicationDbContext.cs
+++ src/Todo.Web/Data/Contexts/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Todo.Web.Data.Base;
 using Todo.Web.Data.Configurations;
@@ -22,6 +23,21 @@ namespace Todo.Web.Data.Contexts
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+            builder.Entity<User>(entity => entity.Property(e => e.Email).IsRequired(false));
+            builder.Entity<User>(entity => entity.ToTable(name: "Users"));
+            builder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
+            builder.Entity<IdentityUserRole<string>>(entity =>
+                entity.ToTable(name: "UserRoles"));
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+                entity.ToTable(name: "UserClaim"));
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+                entity.ToTable("UserLogins"));
+            builder.Entity<IdentityUserToken<string>>(entity =>
+                entity.ToTable("UserTokens"));
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+                entity.ToTable("RoleClaims"));
+
             builder.ApplyConfiguration(new ToDoItemConfiguration());
             builder.ApplyConfiguration(new UserConfiguration());
             builder.ApplyConfiguration(new TagConfiguration());
