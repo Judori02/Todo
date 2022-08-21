2851ec2afbb6d39565c095d507193fd7fe9dcfce
Judori02 <v.steblev2002@gmail.com>
2022-08-21 19:26:26 +0300

    Добавил интерфейс для заметки

---
+++ src/Todo.Web/Infrastructure/Interfaces/IToDoService.cs
@@ -0,0 +1,13 @@
+﻿using Todo.Web.Models;
+using static Todo.Web.Infrastructure.Services.ToDoService;
+
+namespace Todo.Web.Infrastructure.Interfaces
+{
+    public interface IToDoService
+    {
+        Task<Result<Guid>> CreateToDo(CreateToDoViewModel viewModel);
+        Task<Result<ToDoItemViewModel>> GetToDoById(Guid toDoId);
+        Task<Result<ToDoListViewModel>> GetListToDo();
+        Task<Result<Guid>> DoneToDo(Guid toDoId);
+    }
+}
---
+++ src/Todo.Web/Models/Result.cs
@@ -0,0 +1,32 @@
+﻿namespace Todo.Web.Models
+{
+    public class Result<T>
+    {
+        public T? Value { get; set; }
+        public string? Message { get; set; }
+        public bool Succeeded { get; set; }
+
+        internal Result(T value)
+        {
+            Value = value;
+            Succeeded = true;
+        }
+
+        internal Result(string message)
+        {
+            Value = default(T);
+            Message = message;
+            Succeeded = false;
+        }
+
+        public static Result<T> Success(T value)
+        {
+            return new Result<T>(value);
+        }
+
+        public static Result<T> Failure(string errors)
+        {
+            return new Result<T>(errors);
+        }
+    }
+}
